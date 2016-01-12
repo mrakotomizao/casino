2b62e4767a12051d81d2cf1a01c194750b8e679a
mrakotomizao <mrakotomizao@gmail.com>
2016-01-12 16:16:19 +0100

    modification mineur

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/casino-v2';
+$config['base_url'] = NULL;
 
 /*
 |--------------------------------------------------------------------------
