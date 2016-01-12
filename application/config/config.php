c022825aa19359c4466465028d0106d34fb9639b
mrakotomizao <mrakotomizao@gmail.com>
2016-01-12 16:40:50 +0100

    Definition de base_url dans config

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = NULL;
+$config['base_url'] = 'https://projet-houlgate.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
