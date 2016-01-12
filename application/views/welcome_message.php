9137b527da252b40f6aedead342b765460dd7c2d
mrakotomizao <mrakotomizao@gmail.com>
2016-01-12 23:39:42 +0100

    petite correction d'url

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -19,12 +19,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <body>
 <script>
-    (function (d, s, id) {
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '465732286935321',
+            xfbml      : true,
+            version    : 'v2.3'
+        });
+    };
+
+    (function(d, s, id){
         var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.5&appId=465732286935321";
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 </script>
