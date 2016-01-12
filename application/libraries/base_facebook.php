7c7a32e5bccbd0c769d31b2cd86ef3e15e0aaa64
mrakotomizao <mrakotomizao@gmail.com>
2016-01-12 23:32:46 +0100

    petite correction d'url

--- application/libraries/base_facebook.php
+++ application/libraries/base_facebook.php
@@ -748,18 +748,23 @@ abstract class BaseFacebook
      * @return mixed The authorization code, or false if the authorization
      *               code could not be determined.
      */
-    protected function getCode()
-    {
-        if (!isset($_REQUEST['code']) || !isset($_REQUEST['state'])) {
-            return false;
+    protected function getCode() {
+        $server_info = array_merge($_GET, $_POST, $_COOKIE);
+
+        if (isset($server_info['code'])) {
+            if ($this->state !== null &&
+                isset($server_info['state']) &&
+                $this->state === $server_info['state']) {
+
+                // CSRF state has done its job, so clear it
+                $this->state = null;
+                $this->clearPersistentData('state');
+                return $server_info['code'];
+            } else {
+                self::errorLog('CSRF state token does not match one provided.');
+                return false;
+            }
         }
-        if ($this->state === $_REQUEST['state']) {
-            // CSRF state has done its job, so clear it
-            $this->state = null;
-            $this->clearPersistentData('state');
-            return $_REQUEST['code'];
-        }
-        self::errorLog('CSRF state token does not match one provided.');
 
         return false;
     }
