2814a1ec8efe541af39acce5f2ddd73363999dbe
mrakotomizao <mrakotomizao@gmail.com>
2016-01-13 21:57:16 +0100

    tentative de correction d'un bug sur heroku

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,7 +6,7 @@ class Welcome extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->output->enable_profiler(TRUE);
+        /*$this->output->enable_profiler(TRUE);*/
     }
 
     public function login()
