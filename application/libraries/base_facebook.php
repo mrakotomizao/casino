06e477d6aa37f610947b8ab5f411d2c49d87df78
mrakotomizao <mrakotomizao@gmail.com>
2016-01-13 12:05:55 +0100

    tentative de correction d'un bug sur heroku

--- application/libraries/base_facebook.php
+++ application/libraries/base_facebook.php
@@ -146,6 +146,8 @@ abstract class BaseFacebook
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_TIMEOUT => 60,
         CURLOPT_USERAGENT => 'facebook-php-3.2',
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => 2
     );
 
     /**
